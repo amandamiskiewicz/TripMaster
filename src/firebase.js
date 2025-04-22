f31f7090d41528144b04371c8728cb2cda84db3d
amandamiskiewicz <225601@student.uek.krakow.pl>
2025-04-22 23:35:56 +0200

    .env file

--- src/firebase.js
+++ src/firebase.js
@@ -4,25 +4,19 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDx3EgoNVGjwsjvugK7-YwfwxRX2aPSB6k",
-    authDomain: "tripmaster-64cb7.firebaseapp.com",
-    projectId: "tripmaster-64cb7",
-    storageBucket: "tripmaster-64cb7.firebasestorage.app",
-    messagingSenderId: "1015671912746",
-    appId: "1:1015671912746:web:9379e6f43ed18c9fe38653",
-    measurementId: "G-MD5JGE52F8"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
 
 const firebaseApp = initializeApp(firebaseConfig);
 
 const db = getFirestore(firebaseApp);
-
-const storage = getStorage(firebaseApp); 
-
-export { db, storage }; 
-
+const storage = getStorage(firebaseApp);
 const auth = getAuth(firebaseApp);
 
-export { auth };
-
-
+export { db, storage, auth };
